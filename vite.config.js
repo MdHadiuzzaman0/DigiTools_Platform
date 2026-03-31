8090d70c0b1f4c9045e60a6fa05377e62bee755a
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-03-31 14:25:40 +0600

    .

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// const isGitHubPages = process.env.NODE_ENV === 'production';
+// 1
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: isGitHubPages ? '/DigiTools_Platform/' : '/'
+// })
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base:'/'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // যদি আপনি GitHub-এ 'build' করেন, তবে রিপোজিটরির নাম হবে base
+    // অন্যথায় (যেমন Netlify বা Local) এটি রুট '/' হিসেবে কাজ করবে
+    base: mode === 'production' ? '/DigiTools_Platform/' : '/',
+  }
 })
